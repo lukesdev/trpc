1b2b3ef8d429179ba07f4805acf8cc5cabac3cd0
Luke <lukesdev@gmail.com>
2019-12-15 15:42:41 +0000

    add init

---
+++ init.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "latest-amazon-linux2" {
+  most_recent = true
+  owners      = ["137112412989"] # Amazon
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
