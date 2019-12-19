795ddb61571e5d631e7fe0cccd2b2dd28a0b5dd4
Luke <lukesdev@gmail.com>
2019-12-19 21:25:32 +0000

    corrct1

--- main.tf
+++ main.tf
@@ -1,27 +1,35 @@
 provider "aws" {
-  region = "eu-west-2"
-}
-data "aws_ami" "ubuntu" {
-  most_recent = true
+    region = "eu-west-1"
+    access_key = ""
+    secret_key = ""
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+resource "aws_instance" "ec2" { # I called name "ec2", you can change your own name
+  ami = "ami-0dad20bd1b9c8c004" # Image: Ubuntu Server 18.04 LTS (HVM), SSD Volume Type
+  instance_type = "t2.micro" # VM Spec
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
+  key_name = "aws-anthony"
 }
 
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow ssh"
+  description = "only ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+output "showPublicIP" {
+  value = "${aws_instance.ec2.*.public_dns}"
 }
